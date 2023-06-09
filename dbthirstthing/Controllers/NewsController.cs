f92119f200adc0bccade0b49eb4b582cc616dea9
DoraFun <101987178+DoraFun@users.noreply.github.com>
2023-06-10 04:43:02 +0500

    better paggination

--- dbthirstthing/Controllers/NewsController.cs
+++ dbthirstthing/Controllers/NewsController.cs
@@ -1,7 +1,9 @@
 ﻿using dbthirstthing.DataContext;
 using dbthirstthing.Models;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 using static dbthirstthing.Models.NewsViewModel;
 
@@ -10,21 +12,27 @@ namespace dbthirstthing.Controllers
     public class NewsController : Controller
     {
         private readonly ApplicationDbContext db = new ApplicationDbContext();
-        private readonly int pageSize = 3; // количество объектов на страницу
+        private readonly int pageSize = 10; // количество объектов на страницу
 
-        public ActionResult Index(int page = 1)
+        public async Task<ActionResult> Index(int page = 1)
         {
-            int totalNewsCount = db.News.Count();
+            var query = db.News.OrderByDescending(n => n.newsdate);
+            int totalNewsCount = await query.CountAsync();
             int totalPagesCount = (int)System.Math.Ceiling((double)totalNewsCount / pageSize);
 
-            List<NewsModel> newsOnCurrentPage = db.News
-                                                .OrderByDescending(n => n.newsdate)
-                                                .Skip((page - 1) * pageSize)
-                                                .Take(pageSize)
-                                                .ToList();
-            PageInfo pageInfo = new PageInfo { PageNumber = page, PageSize = pageSize, TotalItems = totalNewsCount };
-            IndexViewModel ivm = new IndexViewModel { PageInfo = pageInfo, News = newsOnCurrentPage };
+            var newsOnCurrentPage = await query.Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            var pageInfo = new PageInfo { PageNumber = page, PageSize = pageSize, TotalItems = totalNewsCount };
+            var ivm = new IndexViewModel { PageInfo = pageInfo, News = newsOnCurrentPage };
             return View(ivm);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
